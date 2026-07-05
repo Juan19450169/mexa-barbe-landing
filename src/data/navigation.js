c369dd9622bac180ab2676cbd2f1323b2b50a361
Juan19450169 <l19450169@itz.edu.mx>
2026-07-04 22:53:31 -0600

    Tarea 4: agreggamos navigation a data y react icon

---
+++ src/data/navigation.js
@@ -0,0 +1,29 @@
+const navigation = [
+  {
+    id: 1,
+    label: "Inicio",
+    href: "#hero",
+  },
+  {
+    id: 2,
+    label: "Servicios",
+    href: "#services",
+  },
+  {
+    id: 3,
+    label: "Galería",
+    href: "#gallery",
+  },
+  {
+    id: 4,
+    label: "Horarios",
+    href: "#location",
+  },
+  {
+    id: 5,
+    label: "Contacto",
+    href: "#footer",
+  },
+];
+
+export default navigation;
